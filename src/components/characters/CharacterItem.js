5812b6780c9bb133e00f37b7cb2800276f96264c
Raafat Basheer <raafat.basheer@yahoo.com>
2021-01-09 13:03:41 +0100

    updating the api

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -1,32 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const CharacterItem = ({ item }) => {
-  return (
-    <div className='card'>
-      <div className='card-inner'>
-        <div className='card-front'>
-          <img src={item.img} alt='' />
-        </div>
-        <div className='card-back'>
-          <h1>{item.name}</h1>
-          <ul>
-            <li>
-              <strong>Actor Name:</strong> {item.portrayed}
-            </li>
-            <li>
-              <strong>Nickname:</strong> {item.nickname}
-            </li>
-            <li>
-              <strong>Birthday:</strong> {item.birthday}
-            </li>
-            <li>
-              <strong>Status:</strong> {item.status}
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="card">
+			<div className="card-inner">
+				<div className="card-front">
+					<img src={item.img} alt="" />
+				</div>
+				<div className="card-back">
+					<h1>{item.name}</h1>
+					<ul>
+						<li>
+							<strong>Actor Name:</strong> {item.portrayed}
+						</li>
+						<li>
+							<strong>Nickname:</strong> {item.nickname}
+						</li>
+						<li>
+							<strong>Birthday:</strong> {item.birthday}
+						</li>
+						<li>
+							<strong>Occupation:</strong>{" "}
+							{item.occupation.join(", ")}
+						</li>
+						<li>
+							<strong>Appearance:</strong>{" "}
+							{item.appearance
+								? item.appearance.join("- ")
+								: "None"}
+						</li>
+						<li>
+							<strong>Better Call Saul Appearance:</strong>{" "}
+							{item.better_call_saul_appearance.length === 0
+								? "None"
+								: item.better_call_saul_appearance.join("- ")}
+						</li>
+						<li>
+							<strong>Status:</strong> {item.status}
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default CharacterItem
+export default CharacterItem;
--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,27 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Search = ({ getQuery }) => {
-  const [text, setText] = useState('')
+	const [text, setText] = useState("");
 
-  const onChange = (q) => {
-    setText(q)
-    getQuery(q)
-  }
+	const onChange = (q) => {
+		setText(q);
+		getQuery(q);
+	};
 
-  return (
-    <section className='search'>
-      <form>
-        <input
+	return (
+		<section className="search">
+			<form>
+				{/* <input
           type='text'
           className='form-control'
           placeholder='Search characters'
           value={text}
           onChange={(e) => onChange(e.target.value)}
           autoFocus
-        />
-      </form>
-    </section>
-  )
-}
+        /> */}
 
-export default Search
+				<div class="search-bar">
+					<input
+						type="text"
+						name=""
+						className="search-txt form-control"
+						placeholder="Search characters..."
+						value={text}
+						onChange={(e) => onChange(e.target.value)}
+						autoFocus
+					/>
+					<a className="search-btn">
+						<i class="fas fa-search"></i>
+					</a>
+				</div>
+			</form>
+		</section>
+	);
+};
+
+export default Search;
