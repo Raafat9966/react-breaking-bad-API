2566fd27c801ccd3e08073d29e43efa665830c85
Raafat Basheer <raafat.basheer@yahoo.com>
2021-01-10 11:23:24 +0100

    Updates

---
+++ precache-manifest.79a83421966c0cbfe5658451094ece72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babc7cafedd744f1f67ef7dfc0f4e349",
+    "url": "/react-breaking-bad-API/index.html"
+  },
+  {
+    "revision": "803d3c598c7687d6d369",
+    "url": "/react-breaking-bad-API/static/css/main.b9e56888.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/react-breaking-bad-API/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-breaking-bad-API/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803d3c598c7687d6d369",
+    "url": "/react-breaking-bad-API/static/js/main.926d79fd.chunk.js"
+  },
+  {
+    "revision": "48f7eddfe8825a977949",
+    "url": "/react-breaking-bad-API/static/js/runtime-main.bfb477f7.js"
+  },
+  {
+    "revision": "3dac53f0823692a7ef75d809ad0b8cd8",
+    "url": "/react-breaking-bad-API/static/media/loading.3dac53f0.gif"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/react-breaking-bad-API/static/media/logo.3795f66d.png"
+  }
+]);
